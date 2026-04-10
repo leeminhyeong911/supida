aef4f4eafb738a39b1e2a1033e89c03dffbeeeb2
leeminhyeong911 <lmh010102@naver.com>
2026-04-10 15:51:11 +0900

    add env

--- src/server.js
+++ src/server.js
@@ -11,8 +11,8 @@ import User from "./User.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const PORT = 3000;
-const MONGO_URL = "mongodb://127.0.0.1:27017/supida";
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/supida";
 const app = express();
 const logger = morgan("dev");
 
